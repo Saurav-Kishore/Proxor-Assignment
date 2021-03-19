1db4e5c5bf1e116718f90629a9498bdb9bc1a743
Saurav-Kishore <32749987+Saurav-Kishore@users.noreply.github.com>
2021-03-19 21:21:40 +0530

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class App
+{
+	public static void main(String[] args) {
+		// Create Drawing class instance
+		// and start it's flow
+		Drawing draw = new Drawing();
+		draw.start();
+	}
+}
---
+++ src/Point.java
@@ -0,0 +1,17 @@
+
+/*
+ * Point class stores the x and y
+ * coordinates of a point.
+ */
+class Point
+{
+	// x & y coordinates
+	int x, y;
+	
+	// Contructor
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
